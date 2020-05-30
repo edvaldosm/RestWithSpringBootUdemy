45baf31566719571f73786ad906498d8c6b622f9
Edvaldo Santiago de Matos <edvaldo.santiago@gmail.com>
2020-05-30 00:47:01 -0300

    Criação da calculardora rest

---
+++ 02 ProjetoCalculadora/src/main/java/br/com/erudio/Greeting.java
@@ -0,0 +1,15 @@
+package br.com.erudio;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	
+
+}
