5ad329743b0ed149662521b72664d3903662b501
Edvaldo Santiago de Matos <edvaldo.santiago@gmail.com>
2020-06-01 18:06:34 -0300

    Migration realizada com sucesso

---
+++ 03 ProjetoCrud/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO person(address, first_name, gender, last_name) VALUES('Rua 1', 'Edvaldo', 'MALE', 'Matos');
+INSERT INTO person(address, first_name, gender, last_name) VALUES('Rua 2', 'Lilian', 'FEMALE', 'Matos');
+INSERT INTO person(address, first_name, gender, last_name) VALUES('Rua 3', 'Clara', 'FEMALE', 'Matos');
+INSERT INTO person(address, first_name, gender, last_name) VALUES('Rua 4', 'Laura', 'FEMALE', 'Matos');
